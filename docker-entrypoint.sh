4d2c9bf5bdb6ae8d8d32b841eed6ae88a12603d8
km127pl <km127pl@gmail.com>
2023-04-29 10:26:58 +0200

    Add a volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 if [ "$MYSQL_DATABASE" ]; then
-  mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/import.sql
+  mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/init.sql
 fi
 
 exec "$@"
