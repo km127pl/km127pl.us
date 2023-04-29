9ba5f8bbe6f1592b8404ecf79705e67a1b368d58
km127pl <km127pl@gmail.com>
2023-04-29 10:30:17 +0200

    Revert "Auto import SQL on docker compose"
    This reverts commit 45bd822d93867c0974223611b1bea947281995d4.

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-if [ "$MYSQL_DATABASE" ]; then
-  mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/init.sql
-fi
-
-exec "$@"
