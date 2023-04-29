45bd822d93867c0974223611b1bea947281995d4
km127pl <km127pl@gmail.com>
2023-04-29 10:11:26 +0200

    Auto import SQL on docker compose

---
+++ db/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ "$MYSQL_DATABASE" ]; then
+  mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/import.sql
+fi
+
+exec "$@"
